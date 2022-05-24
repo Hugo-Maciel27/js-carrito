042bcc14a24128946c1b5e0fdd65e9253c7afd17
Hugo Maciel <hugomaciel2711@gmail.com>
2022-05-24 00:18:29 -0300

    cambio de rutas

--- stock.js
+++ stock.js
@@ -6,7 +6,7 @@ const productos = [
         componente: "CPU",
         precio: 5000,
         desc: "Ryzen™ 7 es una CPU de ocho núcleos y 16 subprocesos que está a la altura de las exigencias de los jugadores y creadores",
-        img:"../public/images/51XoylGq9iL._AC_SY450_.jpg",
+        img:"public/images/51XoylGq9iL._AC_SY450_.jpg",
         stock: 3
     },
 
@@ -16,7 +16,7 @@ const productos = [
         componente: "CPU",
         precio: 3000,
         desc: "Ryzen™ 3 es una CPU de ocho núcleos y 16 subprocesos que está a la altura de las exigencias de los jugadores y creadores",
-        img:"../public/images/LD0005368402_2.jpg",
+        img:"public/images/LD0005368402_2.jpg",
         stock: 5
     },
     {
@@ -25,7 +25,7 @@ const productos = [
         componente: "Placa de video",
         precio: 15000,
         desc: "La GPU más rápida del mundo. Obtén la ventaja competitiva que necesitas con GeForce RTX™. Gráficos realistas con un rendimiento increíblemente rápido.",
-        img:"../public/images/placaNvidia3080.jpg",
+        img:"public/images/placaNvidia3080.jpg",
         stock: 2
     },
     {
@@ -34,7 +34,7 @@ const productos = [
         componente: "Placa de video",
         precio: 14000,
         desc: "Potenciada con la arquitectura AMD RDNA™ 2 y equipada con 80 potentes unidades de procesamiento mejoradas, 128MB de la flamante tecnología AMD Infinity Cache y 16GB de memoria GDDR6 dedicada.",
-        img:"../public/images/placa-video-AMD-Radeon-OC-6900-XT.jpeg",
+        img:"public/images/placa-video-AMD-Radeon-OC-6900-XT.jpeg",
         stock: 3
     },
     {
@@ -43,7 +43,7 @@ const productos = [
         componente: "Notebook",
         precio: 12000,
         desc: "Su pantalla LED de 15.6 y 1920x1080 px de resolución te brindará colores más vivos y definidos. Su procesador Intel Core i5 de 4 núcleos, está pensado para aquellas personas generadoras y consumidoras de contenidos.",
-        img:"../public/images/notebook-msi.png",
+        img:"public/images/notebook-msi.png",
         stock: 5
     },
     {
@@ -52,7 +52,7 @@ const productos = [
         componente: "memoria ram",
         precio: 1000,
         desc: "Su capacidad de 8 GB hace de esta memoria un soporte ideal para trabajos con documentos de alta complejidad, navegación en la web con múltiples pestañas, juegos y contenidos multimedia",
-        img:"../public/images/hyperx-fury-rgb-32000mhz.jpg",
+        img:"public/images/hyperx-fury-rgb-32000mhz.jpg",
         stock: 6
     }
     
